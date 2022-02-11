1a1375a03fefe17484ec1b65d6dac1b40ec0288f
Doctor-s-scheduler <vijayamaurya@gmail.com>
2022-02-11 22:09:18 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Second from './second';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Second />
   </React.StrictMode>,
   document.getElementById('root')
 );
